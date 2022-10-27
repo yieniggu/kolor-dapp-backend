6d311ce257d8755d73f75b653700a70a7716dacc
Bastiloko <bastianandresxb@gmail.com>
2022-10-27 09:34:25 -0300

    mainnet-chore: v1.2.2

--- helpers/marketplace.js
+++ helpers/marketplace.js
@@ -6,8 +6,8 @@ const {
 } = require("./web3Common");
 const { newKit, CeloContract } = require("@celo/contractkit");
 
-const web3 = new Web3("https://alfajores-forno.celo-testnet.org");
-// const web3 = new Web3("https://forno.celo.org");
+// const web3 = new Web3("https://alfajores-forno.celo-testnet.org");
+const web3 = new Web3("https://forno.celo.org");
 
 const burnAddress = "0x0000000000000000000000000000000000000000";
 
